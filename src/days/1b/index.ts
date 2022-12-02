197d0ce15df8e05af4cec7217c918af3a9f18ada
Pete Redhead <pete@binderpos.com>
2022-12-02 21:00:48 +1300

    Day 1

--- src/days/1b/index.ts
+++ src/days/1b/index.ts
@@ -2,19 +2,21 @@ import { fileToArray, readFile } from "../../helpers/file";
 const data = fileToArray(readFile(__dirname, "input.txt"));
 
 const solve = () => {
-  let max = 0;
+  let topThree = [0, 0, 0];
   let currentTotal = 0;
   data.forEach((row) => {
     if (row === "") {
-      if (currentTotal > max) {
-        max = currentTotal;
+      const lowestValue = Math.min(...topThree);
+      if (currentTotal > lowestValue) {
+        const lowestPosition = topThree.indexOf(lowestValue);
+        topThree[lowestPosition] = currentTotal;
       }
       currentTotal = 0;
       return;
     }
     currentTotal += Number(row);
   });
-  return max;
+  return topThree.reduce((acc, val) => acc + val, 0);
 };
 
 console.log(solve());
