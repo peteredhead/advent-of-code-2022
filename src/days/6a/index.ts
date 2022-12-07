ab62190864f6d16314467f1edb4d9cf102c133e2
Pete Redhead <pete@binderpos.com>
2022-12-07 17:42:00 +1300

    Day 6

---
+++ src/days/6a/index.ts
@@ -0,0 +1,16 @@
+import { readFile } from "../../helpers/file";
+const data = readFile(__dirname, "input.txt");
+
+const msgLength = 4;
+
+const solve = () => {
+  for (let index = 0; index < data.length; index++) {
+    const group = data.slice(index, index + msgLength);
+    const uniqueChars = [...new Set(group)];
+    if (uniqueChars.length === msgLength) {
+      return index + msgLength;
+    }
+  }
+};
+
+console.log(solve());
