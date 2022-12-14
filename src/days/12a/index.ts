fe2d56cdab16f314a8b666ce96c002fc7c062c2c
Pete Redhead <pete@binderpos.com>
2022-12-15 11:07:01 +1300

    Day 12

---
+++ src/days/12a/index.ts
@@ -0,0 +1,64 @@
+import { fileToArray, readFile } from "../../helpers/file";
+const data = fileToArray(readFile(__dirname, "input.txt"));
+
+const findInGrid = (charToFind: string, rows: number[][]): number =>
+  rows.findIndex((row) => row.includes(charToFind.charCodeAt(0)));
+
+const findInRow = (charToFind: string, row: number[]): number =>
+  row.findIndex((value) => value === charToFind.charCodeAt(0));
+
+const moves = [
+  [1, 0],
+  [0, 1],
+  [-1, 0],
+  [0, -1],
+];
+
+const solve = () => {
+  const queue = [];
+
+  const grid = data.map((row) =>
+    row.split("").map((cell) => cell.charCodeAt(0))
+  );
+  const visitedGrid = data.map((row) => row.split("").map(() => false));
+  const startRow = findInGrid("S", grid);
+  const startColumn = findInRow("S", grid[startRow]);
+  const endRow = findInGrid("E", grid);
+  const endColumn = findInRow("E", grid[endRow]);
+
+  grid[startRow][startColumn] = "a".charCodeAt(0) - 1;
+  grid[endRow][endColumn] = "z".charCodeAt(0) + 1;
+
+  const start = [startRow, startColumn];
+  const end = [endRow, endColumn];
+
+  queue.push([start]);
+  visitedGrid[startRow][startColumn] = true;
+
+  const moveIsInBounds = (row: number, column: number) =>
+    row >= 0 && row < grid.length && column >= 0 && column < grid[0].length;
+
+  while (queue.length > 0) {
+    const path = queue.shift();
+    const pos = path[path.length - 1];
+
+    if (pos[0] == end[0] && pos[1] == end[1]) {
+      return path.concat([end]).length - 2;
+    }
+
+    moves
+      .map(([r, c]) => [pos[0] + r, pos[1] + c])
+      .forEach(([row, column]) => {
+        if (
+          moveIsInBounds(row, column) &&
+          !visitedGrid[row][column] &&
+          grid[row][column] <= grid[pos[0]][pos[1]] + 1
+        ) {
+          visitedGrid[row][column] = true;
+          queue.push(path.concat([[row, column]]));
+        }
+      });
+  }
+};
+
+console.log(solve());
